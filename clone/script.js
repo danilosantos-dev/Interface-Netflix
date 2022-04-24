b6d16a78b558f03caadd6b021dae0d157b96bcc3
Danilo <daanilocarvalho712@gmail.com>
2022-04-24 03:10:59 -0300

    clone v1

---
+++ clone/script.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        500:{
+            items:3
+        },
+        800:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+})
